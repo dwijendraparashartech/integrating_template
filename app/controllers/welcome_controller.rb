d5007d1e728850d27b94ee46233fc5daaf11e8c5
dwijendra parashar <dwijendraparashartech@gmail.com>
2022-12-23 17:27:39 +0530

    Add Integrate Template

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,54 @@
 class WelcomeController < ApplicationController
   def index
   end
+  def dashboard_one
+  end
+  def dashboard_two
+  end
+  def dashboard_three
+  end
+  def dashboard_four
+  end
+  def app_calendar
+  end
+  def app_chat
+  end
+  def app_contacts
+  end
+  def app_file_manager
+  end
+  def app_mail
+  end
+  def page_signin
+  end
+  def page_signup
+  end 
+  def page_verify
+  end
+  def page_forgot
+  end
+  def page_profile_view
+  end
+  def page_connections
+  end
+  def page_groups
+  end
+  def page_events
+  end
+  def page_404
+  end
+  def page_500
+  end
+  def page_503
+  end
+  def page_505
+  end
+  def page_timeline
+  end
+  def page_pricing
+  end
+  def page_help_center
+  end
+  def page_invoice
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,33 @@ Rails.application.routes.draw do
   resources :welcomes
   get 'welcomes/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  #Welcome Routes 
+
+  # get 'welcome/dashboard_one', to: 'welcome#dashboard_one'
+  # get 'welcome/dashboard_two', to: 'welcome#dashboard_two'
+  # get 'welcome/dashboard_three', to: 'welcome#dashboard_three'
+  # get 'welcome/dashboard_four', to: 'welcome#dashboard_four'
+  # get 'welcome/app_calendar', to: 'welcome#app_calendar'
+  # get 'welcome/app_chat', to: 'welcome#app_chat'
+  # get 'welcome/app_contacts', to: 'welcome#app_contacts'
+  # get 'welcome/app_file_manager', to: 'welcome#app_file_manager'
+  # get 'welcome/app_mail', to: 'welcome#app_mail'
+  # get 'welcome/page_signin', to: 'welcome#unknown'
+  # get 'welcome/age_signup', to: 'welcome#age_signup'
+  # get 'welcome/page_verify', to: 'welcome#page_verify'
+  # get 'welcome/page_forgot', to: 'welcome#page_forgot'
+  # get 'welcome/page_profile_view', to: 'welcome#page_profile_view'
+  # get 'welcome/page_connections', to: 'welcome#page_connections'
+  # get 'welcome/page_groups', to: 'welcome#page_groups'
+  # get 'welcome/page_events', to: 'welcome#page_events'
+  # get 'welcome/page_404', to: 'welcome#page_404'
+  # get 'welcome/page_500', to: 'welcome#page_500'
+  # get 'welcome/page_503', to: 'welcome#page_503'
+  # get 'welcome/page_505', to: 'welcome#page_505'
+  # get 'welcome/page_timeline', to: 'welcome#page_timeline'
+  # get 'welcome/page_pricing', to: 'welcome#page_pricing'
+  # get 'welcome/page_invoice', to: 'welcome#page_invoice'
 
   # Defines the root path route ("/")
   root "welcome#index"
